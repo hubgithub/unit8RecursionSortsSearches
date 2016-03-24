a8d87277ee9df5cf059f63aa163812caa40cccf8
hubgithub <HLi@student.naperville203.org>
2016-03-24 15:09:50 -0500

    hfd

--- Koch/WordCount.java
+++ Koch/WordCount.java
@@ -1,16 +1,33 @@
 import java.util.Scanner;
 import java.io.File;
-public class WordCount
+import java.io.FileNotFoundException;
+public class WordCount 
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws FileNotFoundException
     {
-        System.out.print("What is your input file");
-        File inputFile = new File(new Scanner(System.in));
-        Sanner file_scan = new Scanner(inputFile);
-        file_scan.useDelimiter("[^A-Za-z]+");
-        file_scan.
+        File inputFile = new File("./WordFile.txt");
+        Scanner file_scan = new Scanner(inputFile);
+        file_scan.useDelimiter(" ");
+        int word_count = 0;
+        while(file_scan.hasNext())
+        {
+            System.out.println(file_scan.next());
+            
+            word_count+= 1;
+        }
         
+        Scanner charater_scan = new Scanner(inputFile);
+        
+        int count_chars = 0;
+        charater_scan.useDelimiter("[^A-Za-z]+");
+        while(file_scan.hasNext())
+        {
+            count_chars++;
+        }
+        
+        System.out.println("Char Count: " + count_chars);
+        file_scan.close();
+        System.out.println("Word Count: " + word_count);
         
     }
-
 }
